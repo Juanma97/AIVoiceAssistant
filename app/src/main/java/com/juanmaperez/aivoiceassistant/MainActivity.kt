64f037727c13aa9712e8c9de375bed7f1c3ee81f
Juanma97 <juanma97@outlook.com>
2023-05-11 09:07:07 +0100

    feat: added voice recognition

---
+++ app/src/main/java/com/juanmaperez/aivoiceassistant/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.juanmaperez.aivoiceassistant
+
+import android.content.Intent
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.speech.tts.TextToSpeech
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var textToSpeech: TextToSpeech
+
+    companion object {
+        private const val REQUEST_CODE_VOICE_INPUT = 100
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnVoiceInput = findViewById<Button>(R.id.btn_voice_input)
+        btnVoiceInput.setOnClickListener {
+            // Iniciar el reconocimiento de voz
+            startVoiceInput()
+        }
+
+    }
+
+    private fun startVoiceInput() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 1)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        startActivityForResult(intent, REQUEST_CODE_VOICE_INPUT)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE_VOICE_INPUT && resultCode == RESULT_OK && data != null) {
+            val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            val spokenText = result?.get(0)
+
+            textToSpeech.speak("Respuesta de ChatGPT", TextToSpeech.QUEUE_FLUSH, null)
+        }
+    }
+
+
+}

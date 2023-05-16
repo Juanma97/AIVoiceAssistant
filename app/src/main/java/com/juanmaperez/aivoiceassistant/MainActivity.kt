7042d53e3715069859163e383aa3d13ad8c49c1e
Juanma97 <juanma97@outlook.com>
2023-05-16 16:54:00 +0100

    feat: added voice assistant

--- app/src/main/java/com/juanmaperez/aivoiceassistant/MainActivity.kt
+++ app/src/main/java/com/juanmaperez/aivoiceassistant/MainActivity.kt
@@ -4,13 +4,25 @@ import android.content.Intent
 import android.os.Bundle
 import android.speech.RecognizerIntent
 import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Callback
+import okhttp3.Call
+import okhttp3.Response
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
+import java.io.IOException
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private lateinit var textToSpeech: TextToSpeech
+    private val client = OkHttpClient()
 
     companion object {
         private const val REQUEST_CODE_VOICE_INPUT = 100
@@ -20,9 +32,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        textToSpeech = TextToSpeech(this, this)
+
         val btnVoiceInput = findViewById<Button>(R.id.btn_voice_input)
         btnVoiceInput.setOnClickListener {
-            // Iniciar el reconocimiento de voz
             startVoiceInput()
         }
 
@@ -43,7 +56,60 @@ class MainActivity : AppCompatActivity() {
             val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
             val spokenText = result?.get(0)
 
-            textToSpeech.speak("Respuesta de ChatGPT", TextToSpeech.QUEUE_FLUSH, null)
+            getResponse(spokenText) { response ->
+                runOnUiThread {
+                    val jsonObject = JSONObject(response)
+                    val textResponse = jsonObject.getJSONArray("choices").getJSONObject(0).getString("text")
+                    val textViewResponse = findViewById<TextView>(R.id.textview_response)
+                    textViewResponse.text = "Respuesta: $textResponse"
+                    textToSpeech.speak(textResponse, TextToSpeech.QUEUE_FLUSH, null, null)
+                }
+            }
+        }
+    }
+
+    private fun getResponse(question: String?, callback: (String) -> Unit) {
+        val apiKey = "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"
+        val url = "https://api.openai.com/v1/engines/text-davinci-003/completions"
+
+        val requestBody = """
+            {
+                "prompt": "$question",
+                "max_tokens": 1500,
+                "temperature": 0.8
+            }
+        """.trimIndent()
+
+        val textViewQuestion = findViewById<TextView>(R.id.textview_question)
+        textViewQuestion.text = "Pregunta: $question"
+
+        val request = Request.Builder()
+            .url(url)
+            .addHeader("Content-Type", "application/json")
+            .addHeader("Authorization", "Bearer $apiKey")
+            .post(requestBody.toRequestBody("application/json".toMediaTypeOrNull()))
+            .build()
+
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("error", "API fail", e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                callback.invoke(body ?: "")
+            }
+        })
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = textToSpeech.setLanguage(Locale.getDefault())
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("MyActivity", "Language not supported")
+            }
+        } else {
+            Log.e("MyActivity", "Initialization failed")
         }
     }
 }
